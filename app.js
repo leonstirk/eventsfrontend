ea384fdf048867e9958dfe87a18079d2744e47ce
leonstirk <leon.stirkwang@protonmail.com>
2025-02-14 21:30:03 +1300

    auto-backup

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://tdmukcers6.execute-api.ap-southeast-2.amazonaws.com/events";
+const API_URL = "https://tdmukcers6.execute-api.ap-southeast-2.amazonaws.com/events?lat=40.7580&lon=-73.9855&radius=5200"
 
 function fetchEvents() {
     fetch(API_URL)
